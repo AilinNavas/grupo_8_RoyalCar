627e08f8f0a6d162f73be96b65bd90bd914b80a0
pachomoncayo <pachomoncayo@yahoo.com>
2023-01-30 16:41:26 -0500

    Se implementa la estructura MVC, como se explicó en clases anteriores

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require ('express');
+const path = require('path');
+const app = express();
+const publicPath = path.join(__dirname, './public');
+app.use(express.static(publicPath));
+app.get('/',(req, res)=>{
+    res.sendFile(path.join(__dirname, './views/productDetail.html'));
+});
+
+const port = 3000;
+app.listen(port, ()=>{
+    console.log(`Server started on http://localhost:${port}`);
+});
