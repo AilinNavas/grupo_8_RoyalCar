f618376f1d58c5e1e2741b4c6a384794d1837a0b
AilinNavas <navasailin18@gmail.com>
2023-01-31 09:31:29 -0300

    Agregue ruta y archivo index.html

--- app.js
+++ app.js
@@ -3,6 +3,11 @@ const path = require('path');
 const app = express();
 const publicPath = path.join(__dirname, './public');
 app.use(express.static(publicPath));
+
+app.get('/',(req, res)=>{
+    res.sendFile(path.join(__dirname, './views/index.html'));
+});
+
 app.get('/',(req, res)=>{
     res.sendFile(path.join(__dirname, './views/productDetail.html'));
 });
