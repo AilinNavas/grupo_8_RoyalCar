0d82a31fbda87f5eb39fc7038fd13122e750d5e5
walterB97 <wbarboza97@gmail.com>
2023-02-01 16:27:06 -0300

    Agrego Register.html

--- app.js
+++ app.js
@@ -11,6 +11,9 @@ app.get('/',(req, res)=>{
 app.get('/',(req, res)=>{
     res.sendFile(path.join(__dirname, './views/productDetail.html'));
 });
+app.get('/register',(req, res)=>{
+    res.sendFile(path.join(__dirname, './views/register.html'));
+});
 
 const port = 3000;
 app.listen(port, ()=>{
