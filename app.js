d4c49318217d6536d4d02f4e637ad07326597d73
Fpiccinini1 <francopiccinini02@gmail.com>
2023-02-06 21:51:44 -0300

    get login

--- app.js
+++ app.js
@@ -15,6 +15,9 @@ app.get('/register',(req, res)=>{
     res.sendFile(path.join(__dirname, './views/register.html'));
 });
 
+app.get('/login',(req, res)=>{
+    res.sendFile(path.join(__dirname, './views/login.html'));
+});
 const port = 3000;
 app.listen(port, ()=>{
     console.log(`Server started on http://localhost:${port}`);
