47cded4f12ac5a7dfc62580b5f896ee0322a5f6b
Your Name <you@example.com>
2023-02-08 10:31:01 -0300

    productCart

--- app.js
+++ app.js
@@ -14,6 +14,9 @@ app.get('/',(req, res)=>{
 app.get('/register',(req, res)=>{
     res.sendFile(path.join(__dirname, './views/register.html'));
 });
+app.get('/productCart',(req, res)=>{
+    res.sendFile(path.join(__dirname, './views/productCart.html'));
+});
 
 const port = 3000;
 app.listen(port, ()=>{
