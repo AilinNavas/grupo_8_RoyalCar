bee02f835f0a69248bfa084f340aca887ea2b765
AilinNavas <navasailin18@gmail.com>
2023-02-15 23:46:17 -0300

    cambio de rama

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ app.get('/index',(req, res)=>{
 app.get('/producto',(req, res)=>{
     res.sendFile(path.join(__dirname, './views/productDetail.html'));
 });
-app.get('/registro',(req, res)=>{
+app.get('/register',(req, res)=>{
     res.sendFile(path.join(__dirname, './views/register.html'));
 });
 app.get('/productCart',(req, res)=>{
