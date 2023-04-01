a4059ceabf5e2193de539d3062b47657229d2df6
AilinNavas <navasailin18@gmail.com>
2023-03-31 23:26:03 -0300

    Se crea la funcionalidad de registro de usuaris

--- src/controllers/usersController.js
+++ src/controllers/usersController.js
@@ -1,9 +1,40 @@
 const path = require('path');
+const fs = require('fs');
+const bcrypt = require('bcryptjs');
+
+
+const usersFilePath = path.join(__dirname, '../database/users.json');
+function getUsers() {
+	return JSON.parse(fs.readFileSync(usersFilePath, 'utf-8'));
+}
+
 
 const controller = {
     register: (req, res) => {
         res.render(path.join(__dirname, '../views/register.ejs'))
     },
+     
+    createUser: (req, res) => {
+     
+        const image = req.file ? req.file.filename : 'default-image.png';
+		const users = getUsers();
+		const newUsers = {
+			id: users[users.length - 1].id + 1,
+			name: req.body.name,
+			last_name: req.body.last_name,
+			email: req.body.email,
+            image,
+			category: req.body.category,
+			password: bcrypt.hashSync(req.body.password, 10),
+            confirm_password:bcrypt.hashSync(req.body.confirm_password, 10) 
+		
+		};
+		users.push(newUsers);
+		fs.writeFileSync(usersFilePath, JSON.stringify(users, null, 2));
+		res.redirect('/');
+    },
+
+
     login: (req, res) => {
         res.render(path.join(__dirname, '../views/login.ejs'))
     }
--- src/routes/users.js
+++ src/routes/users.js
@@ -1,10 +1,25 @@
 const express = require("express");
+const multer = require("multer");
+const path = require("path");
+
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, path.resolve(__dirname, "../public/images/users"));
+    },
+    filename: (req, file, cb) => {
+        cb(null, Date.now() + '-' + file.originalname);
+    }
+});
+
+
+const upload = multer({ storage });
 
 const usersController = require("../controllers/usersController");
 
 const router = express.Router();
 
 router.get("/register", usersController.register );
+router.post("/register",upload.single('usersImage'), usersController.createUser);
 
 router.get("/login", usersController.login );
 
