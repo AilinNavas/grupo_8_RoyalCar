c6481f454db88f488bdb01011aaf6c3826a2a7a8
AilinNavas <navasailin18@gmail.com>
2023-04-07 11:24:25 -0300

    Se terminan las validaciones del formulario de registro

--- src/controllers/usersController.js
+++ src/controllers/usersController.js
@@ -1,6 +1,7 @@
 const path = require('path');
 const fs = require('fs');
 const bcrypt = require('bcryptjs');
+const { validationResult } = require('express-validator');
 
 
 const usersFilePath = path.join(__dirname, '../database/users.json');
@@ -10,34 +11,39 @@ function getUsers() {
 
 
 const controller = {
-    register: (req, res) => {
-        res.render(path.join(__dirname, '../views/register.ejs'))
-    },
-     
-    createUser: (req, res) => {
-     
-        const image = req.file ? req.file.filename : 'default-image.png';
+	register: (req, res) => {
+		res.render('register');
+	},
+
+	createUser: (req, res) => {
+		const resultValidation = validationResult(req);
+
+		if (resultValidation.errors.length > 0) {
+			res.render('register', { errors: resultValidation.mapped(), oldData: req.body });
+		}
+
+		const image = req.file ? req.file.filename : 'default-image.png';
 		const users = getUsers();
 		const newUsers = {
 			id: users[users.length - 1].id + 1,
 			name: req.body.name,
 			last_name: req.body.last_name,
 			email: req.body.email,
-            image,
+			image,
 			category: req.body.category,
 			password: bcrypt.hashSync(req.body.password, 10),
-            confirm_password:bcrypt.hashSync(req.body.confirm_password, 10) 
-		
+			confirm_password: bcrypt.hashSync(req.body.confirm_password, 10)
+
 		};
 		users.push(newUsers);
 		fs.writeFileSync(usersFilePath, JSON.stringify(users, null, 2));
-		res.redirect('/');
-    },
+		res.redirect('login');
+	},
 
 
-    login: (req, res) => {
-        res.render(path.join(__dirname, '../views/login.ejs'))
-    }
+	login: (req, res) => {
+		res.render(path.join(__dirname, '../views/login.ejs'))
+	}
 
 };
 
--- src/routes/users.js
+++ src/routes/users.js
@@ -16,10 +16,36 @@ const upload = multer({ storage });
 
 const usersController = require("../controllers/usersController");
 
+const { body } = require("express-validator");
+
+const validationsRegister = [
+    body("name")
+        .notEmpty().withMessage('*Debes completar con tu nombre'),
+    body("last_name")
+        .notEmpty().withMessage('*Debes completar con tu apellido'),
+    body("email")
+        .notEmpty().withMessage('*Debes completar con un correo electronico').bail()
+        .isEmail().withMessage('*Debes completar con un correo electronico válido'),
+    body("category")
+        .notEmpty().withMessage('*Debes seleccionar una categoria'),
+    body("password")
+        .notEmpty().withMessage('*Debes completar con una contraseña').bail()
+        .isLength({ min: 8 }).isAlphanumeric().withMessage('La contraseña debe ser alfanumérica y tener mínimo 8 caractéres'),
+    body("confirm_password")
+        .notEmpty().withMessage('*Debes confirmar tu contraseña'),
+    body("usersImage").custom((value, { req }) => {
+        let file = req.file;
+        if(!file) {
+            throw new Error('*Debes subir una imagen de perfil')
+        }
+        return true;
+    })
+]
+
 const router = express.Router();
 
 router.get("/register", usersController.register );
-router.post("/register",upload.single('usersImage'), usersController.createUser);
+router.post("/register",upload.single('usersImage'), validationsRegister, usersController.createUser);
 
 router.get("/login", usersController.login );
 
