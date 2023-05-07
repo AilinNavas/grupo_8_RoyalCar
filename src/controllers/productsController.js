eb6f5078b63731cf777b9ce3a62ff70b7ac48ce7
walterB97 <wbarboza97@gmail.com>
2023-05-06 21:11:04 -0300

    Empezando el Crud

--- src/controllers/productsController.js
+++ src/controllers/productsController.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const fs = require('fs');
+const db = require('../database/models');
 
 const productsFilePath = path.join(__dirname, '../database/products.json');
 function getProducts() {
--- src/database/seeders/20230502144431-ProductsSeeder.js
+++ src/database/seeders/20230502144431-ProductsSeeder.js
@@ -17,49 +17,10 @@ module.exports = {
         year: 2020,
         description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.',
         price: 256,
-        image : 'ferrari-monza.jpg' ,
-        products_brands_id: 1 
+        image : 'ferrari-monza.jpg',
+        products_brands_id:1 
        },
-       {
-        model: 'Veneno',
-        year: 2022,
-        description:'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.' ,
-        price: 350 ,
-        image : 'labo-veneno3.jpg',
-        products_brands_id: 2 
-       },
-       {
-        model: 'Sian',
-        year: 2021,
-        description:'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.' ,
-        price: 445,
-        image : 'labo-sian6.jpg',
-        products_brands_id: 2
-       },
-       {
-        model: '765LT spider',
-        year: 2021,
-        description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.' ,
-        price: 455,
-        image : 'mclaren4.jpg',
-        products_brands_id: 7
-       },
-       {
-        model: 'Dawn Black Badge',
-        year: 2020,
-        description:'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.' ,
-        price: 356,
-        image : 'roy.jpg',
-        products_brands_id: 4
-       },
-       {
-        model: '918 Spyder',
-        year: 2023 ,
-        description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.' ,
-        price: 359 ,
-        image :'pors.jpg',
-        products_brands_id: 5 
-       }], {});
+       ], {});
   },
 
   async down (queryInterface, Sequelize) {
