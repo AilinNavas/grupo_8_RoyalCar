ea6fb3400736f952b51caf91f1e7a03e2bad6c1d
AilinNavas <navasailin18@gmail.com>
2023-05-09 12:09:07 -0300

    Crud de productos en proceso

--- src/controllers/productsController.js
+++ src/controllers/productsController.js
@@ -2,95 +2,115 @@ const path = require('path');
 const fs = require('fs');
 const db = require('../database/models');
 
-const productsFilePath = path.join(__dirname, '../database/products.json');
-function getProducts() {
-	return JSON.parse(fs.readFileSync(productsFilePath, 'utf-8'));
-}
-
 
 
 const controller = {
 	//(GET) Mostrar todos los productos
 	products: (req, res) => {
-		const products = getProducts();
-		res.render('products', { products });
+
+		db.Product.findAll({ include: { all: true } })
+			.then((products) => {
+				res.render('products', { products });
+			})
+			.catch((err) => {
+				res.send(err);
+			});
+	},
+	productDetail: async (req, res) => {
+		try {
+			const id = req.params.id;
+			const product = await db.Product.findOne({ include: ['brand', 'colors'], where: { id } });
+			if (!product) {
+				return res.status(404).json({ message: 'Producto no encontrado' });
+			}
+			res.render('detail', { product });
+		} catch (error) {
+			res.send(error);
+		}
 	},
 
 	//(GET) Mostrar formulario de creacion de producto
-	formCreate: (req, res) => {
-		res.render(path.join(__dirname, '../views/formCreate'))
+	create: async (req, res) => {
+		const brands = await db.Brand.findAll();
+
+		const colors = await db.Color.findAll();
+
+		res.render('formCreate', { brands, colors });
 	},
 
-	store: (req, res) => {
-		const image = req.file ? req.file.filename : 'default-image.png';
-		const products = getProducts();
-		const newProduct = {
-			id: products[products.length - 1].id + 1,
-			brand: req.body.brand,
-			model: req.body.model,
-			year: req.body.year,
-			description: req.body.description,
-			price: req.body.price,
-			color: req.body.color,
-			image
-		};
-		products.push(newProduct);
-		fs.writeFileSync(productsFilePath, JSON.stringify(products, null, 2));
-		res.redirect('/products');
+	store: async (req, res) => {
+		try {
+			const image = req.file ? req.file.filename : 'default-image.png';
+			const productToCreate = {
+				model: req.body.model,
+				year: req.body.year,
+				description: req.body.description,
+				price: req.body.price,
+				color: req.body.color,
+				image,
+				products_brands_id: req.body.products_brands_id
+			};
+
+			await db.Product.create(productToCreate);
+
+			res.redirect('products')
+
+		} catch (error) {
+			res.send(error);
+		}
 	},
 
+
+	edit: async (req, res) => {
+		const id = req.params.id;
+
+		const brands = await db.Brand.findAll();
+
+		const colors = await db.Color.findAll();
+
+		const product = await db.Product.findOne({ include: { all: true }, where: { id } });
+
+		res.render("productEdit", { product, brands, colors });
+	},
+
+
+	update: async (req, res) => {
+		try {
+			const image = req.file ? req.file.filename : 'default-image.png';
+			const productToUpdate = {
+				model: req.body.model,
+				year: req.body.year,
+				description: req.body.description,
+				price: req.body.price,
+				color: req.body.color,
+				image,
+				products_brands_id: req.body.products_brands_id
+			};
+
+			await db.Product.update(productToUpdate, { where: { id: req.params.id }, force: true });
+			res.redirect('/products');
+
+		} catch (error) {
+			res.send(error);
+		}
+
+	},
+	destroy: async (req, res) => {
+
+		try {
+			const id = req.params.id;
+			await db.Product.destroy({ where: { id } });
+			res.redirect('/products');
+		}
+		catch (error) {
+			res.send(error)
+		}
+
+	},
 	productCart: (req, res) => {
-		res.render(path.join(__dirname, '../views/productCart'))
+		res.render(path.join(__dirname, 'productCart'))
 	},
-
-	productDetail: (req, res) => {
-		const id = req.params.id;
-		const products = getProducts();
-		const product = products.find(element => element.id == id);
-		res.render('detail', { product });
-	},
-
-
-	edit: (req, res) => {
-		const id = req.params.id;
-		const products = getProducts();
-		const product = products.find(element => element.id == id);
-		
-		res.render('productEdit', { productToEdit: product });
-	},
-	update: (req, res) => {
-		const id = req.params.id;
-		const products = getProducts();
-		const productIndex = products.findIndex(element => element.id == id);
-		const image = req.file ? req.file.filename : products[productIndex].image;
-		products[productIndex] = {
-			...products[productIndex],
-			brand: req.body.brand,
-			model: req.body.model,
-			year: req.body.year,
-			power: req.body.power,
-			price: req.body.price,
-			color: req.body.color,
-			image
-
-		};
-
-		fs.writeFileSync(productsFilePath, JSON.stringify(products, null, 2));
-		res.redirect('/products');
-
-
-
-	},
-	destroy: (req, res) => {
-		const products = getProducts();
-	
-		const productIndex = products.findIndex(element => element.id == req.params.id);
-		products.splice(productIndex, 1);
-		fs.writeFileSync(productsFilePath, JSON.stringify(products, null, 2));
-		
-		res.redirect('/products');
-	}
 
 }
-	module.exports = controller;
+module.exports = controller;
 
